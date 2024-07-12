decacec2f248f598d3f4b0eb60cb02f8bff76fac
waiting <1661926154@qq.com>
2024-07-12 19:28:19 +0800

    feat(boot): modify properties on root.class.ts
    add base.controller.ts

--- packages/boot/src/core/root.class.ts
+++ packages/boot/src/core/root.class.ts
@@ -1,27 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import assert from 'node:assert/strict'
-import type { IncomingHttpHeaders } from 'node:http'
 
 import { App, Config, Inject, MidwayEnvironmentService } from '@midwayjs/core'
 import { ValidateService } from '@midwayjs/validate'
 // import { ILogger as Logger } from '@midwayjs/logger'
-import {
-  FetchService,
-  Headers,
-  HeadersInit,
-  ResponseData,
-} from '@mwcp/fetch'
+import { HeadersInit } from '@mwcp/fetch'
 import { JwtComponent } from '@mwcp/jwt'
 import { KoidComponent } from '@mwcp/koid'
-import { AttrNames, TraceLogger, TraceService } from '@mwcp/otel'
+import { AttrNames } from '@mwcp/otel'
 import { formatDateTime, MyError } from '@mwcp/share'
 
-import type {
+import {
   Application,
   Context,
-  FetchOptions,
-  JsonResp,
-  NpmPkg,
+  type FetchOptions,
+  type NpmPkg,
 } from '##/lib/index.js'
 
 
@@ -33,16 +26,9 @@ export class RootClass {
 
   @Inject() readonly environmentService: MidwayEnvironmentService
 
-  @Inject() readonly fetchService: FetchService
-
-  /** TraceLogger */
-  @Inject() readonly logger: TraceLogger
-
   @Inject() readonly koid: KoidComponent
 
   @Inject() readonly jwt: JwtComponent
-
-  @Inject() readonly traceService: TraceService
 
   @Config() readonly pkg: NpmPkg
   @Config() readonly globalErrorCode: Record<string | number, string | number>
@@ -60,7 +46,6 @@ export class RootClass {
     return this.koid.idGenerator
   }
 
-  /* c8 ignore next */
   /**
    * Generate an RxRequestInit variable,
    * @default
@@ -90,172 +75,6 @@ export class RootClass {
     return args
   }
 
-  /* c8 ignore start */
-
-  /**
-   * 请求和返回类型都是 JSON 格式，
-   * 返回类型为 `JsonResp` 结构
-   */
-  fetch<T extends ResponseData>(options: FetchOptions): Promise<JsonResp<T>> {
-
-    const opts: FetchOptions = {
-      ...this.initFetchOptions,
-      ...options,
-      headers: this.genFetchHeaders(options.headers),
-    }
-    return this.fetchService.fetch(opts)
-  }
-
-  /**
-   * 请求和返回类型都是 JSON 格式，
-   * 返回类型为 `JsonResp` 结构
-   */
-  getJson<T extends ResponseData>(
-    url: string,
-    options?: FetchOptions,
-  ): Promise<JsonResp<T>> {
-
-    const opts: FetchOptions = {
-      ...this.initFetchOptions,
-      ...options,
-      headers: this.genFetchHeaders(options?.headers),
-    }
-    return this.fetchService.get(url, opts)
-  }
-
-  /**
-   * 请求和返回类型都是 JSON 格式，
-   * 返回类型为 `JsonResp` 结构
-   */
-  postJson<T extends ResponseData>(
-    url: string,
-    options?: FetchOptions,
-  ): Promise<JsonResp<T>> {
-
-    const opts: FetchOptions = {
-      ...this.initFetchOptions,
-      ...options,
-      headers: this.genFetchHeaders(options?.headers),
-    }
-    return this.fetchService.post(url, opts)
-  }
-
-
-  /**
-   * 请求和返回类型都是 JSON 格式，
-   * 返回类型为自定义结构
-   */
-  fetchCustom<T>(options: FetchOptions): Promise<T> {
-
-    const opts: FetchOptions = {
-      ...this.initFetchOptions,
-      ...options,
-      headers: this.genFetchHeaders(options.headers),
-    }
-    return this.fetchService.fetch(opts) as Promise<T>
-  }
-
-  /**
-   * 请求和返回类型都是 JSON 格式，
-   * 返回类型为自定义结构
-   */
-  getCustomJson<T>(
-    url: string,
-    options?: FetchOptions,
-  ): Promise<T> {
-
-    const opts: FetchOptions = {
-      ...this.initFetchOptions,
-      ...options,
-      headers: this.genFetchHeaders(options?.headers),
-    }
-    return this.fetchService.get(url, opts) as Promise<T>
-  }
-
-  /**
-   * 请求和返回类型都是 JSON 格式，
-   * 返回类型为自定义结构
-   */
-  postCustomJson<T>(
-    url: string,
-    options?: FetchOptions,
-  ): Promise<T> {
-
-    const opts: FetchOptions = {
-      ...this.initFetchOptions,
-      ...options,
-      headers: this.genFetchHeaders(options?.headers),
-    }
-    return this.fetchService.post(url, opts) as Promise<T>
-  }
-
-  /* c8 ignore stop */
-
-  /**
-   * 返回类型为字符串
-   */
-  async getText<T extends string = string>(
-    url: string,
-    options?: FetchOptions,
-  ): Promise<T> {
-
-    const opts: FetchOptions = {
-      ...this.initFetchOptions,
-      ...options,
-      headers: this.genFetchHeaders(options?.headers),
-      dataType: 'text',
-    }
-    const ret = await this.fetchService.get<T>(url, opts)
-    return ret
-  }
-
-  /**
-   * 根据输入 http headers 生成 Headers,
-   * @returns Headers 默认不包括以下字段
-   *   - host: 当前服务器地址
-   *   - connection
-   *   - content-length
-   */
-  genFetchHeaders(
-    headers?: HeadersInit | IncomingHttpHeaders | undefined,
-    excludes: string[] = ['host', 'connection', 'content-length'],
-  ): Headers {
-
-    const ret = new Headers(this.initFetchOptions.headers)
-    if (! headers) {
-      return ret
-    }
-
-    if (headers instanceof Headers) {
-      headers.forEach((val, key) => {
-        if (Array.isArray(excludes) && excludes.includes(key)) { return }
-        ret.set(key, val)
-      })
-      return ret
-    }
-    else if (Array.isArray(headers)) { // [string, string][]
-      headers.forEach(([key, val]) => {
-        if (! key) { return }
-        if (Array.isArray(excludes) && excludes.includes(key)) { return }
-        if (typeof val === 'undefined') { return }
-        ret.set(key, val)
-      })
-      return ret
-    }
-    else if (typeof headers === 'object') { // IncomingHttpHeaders
-      Object.keys(headers).forEach((key) => {
-        if (Array.isArray(excludes) && excludes.includes(key)) { return }
-        const data = headers[key]
-        if (typeof data === 'undefined') { return }
-        const value = Array.isArray(data) || typeof data === 'object' // last for ReadonlyArray
-          ? data.join(',')
-          : data
-        ret.set(key, value)
-      })
-    }
-
-    return ret
-  }
 
   getJwtPayload<T = unknown>(ctx: Context): T {
     assert(ctx, 'ctx is required')
