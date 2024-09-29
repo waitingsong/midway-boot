e7511ea4d6cf61b730232c3a63cd95352907b3e7
waiting <1661926154@qq.com>
2024-09-29 20:15:01 +0800

    ci: 8

--- .scripts/ci/ci-init-db.sh
+++ .scripts/ci/ci-init-db.sh
@@ -5,15 +5,14 @@ psql -V
 # netstat -tunpl
 # dig postgres
 
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "SELECT version();"
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "SHOW TIMEZONE;"
 
 echo -e "\n"
 
-dig postgres
-dig pgmq
+psql -h $PGMQ_HOST -p $PGMQ_PORT -U$PGMQ_USER -d $PGMQ_DB -c "SELECT version();"
 psql -h $PGMQ_HOST -p $PGMQ_PORT -U$PGMQ_USER -d $PGMQ_DB -c "SHOW TIMEZONE;"
 
-# SQL_DIR='./packages/taskman/database/'
 SQL_DIR='./node_modules/@waiting/pgmq-js/database/'
 
 cd "$SQL_DIR"
